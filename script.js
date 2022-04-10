726ba6c92665fb8533470b6d00e81cae9368db4c
JustFast <justfast@github.com>
2022-04-09 17:44:32 -0700

    added popups

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const wsurl = "placeholder";
-
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const boardcanvas = document.createElement("canvas");
 const boardctx = boardcanvas.getContext("2d");
+const popup_container = document.getElementsByClassName("popup_container")[0];
+const popup_text = document.getElementById("popup_text");
 
 let game_board = [
     [[0, -1], [0, -1], [0, -1], [0, -1], [0, -1], [0, -1], [0, -1], [0, -1]],
@@ -23,9 +23,9 @@ async function draw_board() {
     for (let i = 0; i < game_board.length; i++) {
         for (let j = 0; j < game_board[i].length; j++) {
             if (game_board[i][j][0] != 0 && game_board[i][j][1] != -1) { // Aspect you better not screw me over
-                let piece_str = game_board[i][j][0] + '_' + game_board[i][j][1] + ".png";
+                let piece_str = `Pieces/${game_board[i][j][0]}_${game_board[i][j][1]}".png`;
                 let piece = new Image();
-                piece.src = "Pieces/" + piece_str;
+                piece.src = piece_str;
                 await piece.decode(); // Another stupid hacky solution
                 ctx.drawImage(
                     piece,
@@ -70,19 +70,58 @@ window.onresize = function () {
 }
 window.onresize();
 
-const ws = new WebSocket(wsurl);
-
-ws.onopen = function (e) {
-    ws.send(JSON.stringify({
-        "type": "join",
-        "intent": "spectator"
-    }));
+function popup(ms, text) {
+    popup_text.innerHTML = text;
+    popup_container.style.display = "block";
+    popup_container.style.animation = "0.5s pop_out";
+    setTimeout(function () {
+        popup_container.style.animation = "0.5s pop_in";
+        popup_container.onanimationend = function () {
+            popup_container.style.display = "none";
+        }
+    }, ms);
 }
 
-ws.onmessage = function (e) {
-    let obj = JSON.parse(e.data);
-    if (obj.type == "game_board") {
-        game_board = obj.board;
-        draw_board();
+function connect() {
+    popup(1000, "Attempting to connect...");
+
+    let ws;
+    try {
+        ws = new WebSocket(document.getElementById("wsurl").value);
+    } catch (e) {
+        popup(3000, "Failed to connect (see console for error)");
+        console.log(e);
+        return;
+    }
+
+    ws.onopen = function (e) {
+        ws.send(JSON.stringify({
+            "type": "join",
+            "intent": "spectator"
+        }));
+    }
+
+    ws.onmessage = function (e) {
+        let obj = JSON.parse(e.data);
+        switch (obj.type) {
+            case "game_board":
+                game_board = obj.board;
+                draw_board();
+                break;
+            case "game_end":
+                let winner_str = (obj.winner == 0) ? "black" : "white";
+                popup(5000, `${winner_str} won the game due to ${obj.reason}`);
+                break;
+            case "game_begin":
+                popup(1000, "Let the game begin!");
+                break;
+        }
+    }
+
+    ws.onerror = function (e) {
+        popup(3000, "WS ERR (see console for error)");
+        console.log(e);
     }
 }
+
+connect();
